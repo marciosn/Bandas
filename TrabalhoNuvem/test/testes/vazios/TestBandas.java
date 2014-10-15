1d987a040d69c135eac4156e5c8e5615ec8a6741
marciosn <marciosouzanobre@gmail.com>
2014-10-15 19:09:56 -0300

    adicionando testes vazios apenas para visualizar os dados no jenkins

---
+++ TrabalhoNuvem/test/testes/vazios/TestBandas.java
@@ -0,0 +1,60 @@
+package testes.vazios;
+
+import junit.framework.TestCase;
+
+public class TestBandas extends TestCase {
+	
+	public void test1(){
+		assertTrue(true);
+	}
+	public void test2(){
+		assertTrue(true);
+	}
+	public void test3(){
+		assertTrue(true);
+	}
+	
+	public void test4(){
+		assertTrue(true);
+	}
+	public void test5(){
+		assertTrue(true);
+	}
+	public void test6(){
+		assertTrue(true);
+	}
+	public void test7(){
+		assertTrue(true);
+	}
+	public void test8(){
+		assertTrue(true);
+	}
+	public void test9(){
+		assertTrue(true);
+	}
+	public void test10(){
+		assertTrue(true);
+	}
+	public void test11(){
+		assertTrue(true);
+	}
+	public void test12(){
+		assertTrue(false);
+	}
+	public void test13(){
+		assertTrue(false);
+	}
+	public void test14(){
+		assertTrue(false);
+	}
+	public void test15(){
+		assertTrue(false);
+	}
+	public void test16(){
+		assertTrue(false);
+	}
+	public void test17(){
+		assertTrue(false);
+	}
+
+}
